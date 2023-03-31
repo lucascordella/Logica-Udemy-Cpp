39d95658db9998be0c452da2cdc41b9a5949f9e2
Lucas Cordella <100384378+lucascordella@users.noreply.github.com>
2023-03-31 16:49:33 -0300

    Add files via upload

---
+++ problema_menor_de_tresc++/main.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int primeiro, segundo, terceiro, menor;
+
+    cout << "Primeiro valor: ";
+    cin >> primeiro;
+    cout << "Segundo valor: ";
+    cin >> segundo;
+    cout << "Terceiro valor: ";
+    cin >> terceiro;
+
+    if (primeiro < segundo && primeiro < terceiro){
+        menor = primeiro;
+    }
+    else if (segundo < primeiro && segundo < terceiro){
+        menor = segundo;
+    }
+    else {
+        menor = terceiro;
+    }
+
+    cout << "MENOR = " << menor << endl;
+
+    return 0;
+}
